d5e9f86d29208b063f9c94b6a8d424ce1fb04a7c
Archi <archivirani01@gmail.com>
2026-04-25 17:50:36 +0530

    sidebar ui

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Sidebar } from './components/sidebar/sidebar';
+import { Header } from './components/header/header';
+import { MainContent } from './components/main-content/main-content';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ Sidebar, Header, MainContent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
