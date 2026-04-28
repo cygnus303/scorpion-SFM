21ee3ae7728c47953e8b1bab1819707119a7e8a1
Archi <archivirani01@gmail.com>
2026-04-28 16:45:30 +0530

    SFM ui

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Sidebar } from './components/sidebar/sidebar';
 import { Header } from './components/header/header';
 import { MainContent } from './components/main-content/main-content';
+import { Login } from './components/login/login';
 
 @Component({
   selector: 'app-root',
-  imports: [ Sidebar, Header, MainContent],
+  imports: [ Sidebar, Header, MainContent, Login],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    // apiUrl: 'https://localhost:44320/api/v1/',
+    apiUrl: 'https://sfmuatapi.cygnux.in/api/v1/',
+    env: 'Dev'
+}
